7ce1517887fa227049b3e690c4329a3724d37032
compsci <compsci@compsci>
2017-10-20 10:43:03 -0600

    ALL THE THINGS
    added readme, started work on runhere and cipher system.

--- src/caesar/CaesarCipher.java
+++ src/caesar/CaesarCipher.java
@@ -2,4 +2,44 @@ package caesar;
 
 public class CaesarCipher {
 
+	private String alpha = "abcdefghijklmnopqrstuvwxyz";
+	
+	
+	
+	public String encode(String vanillaText, int key) {
+		
+		String chocolateText = "";
+		
+		/**
+		System.out.println("Alpha length: " + alpha.length());
+		System.out.println("Alpha first char: " + alpha.charAt(0));
+		System.out.println("Alpha last char: " + alpha.charAt(alpha.length() - 1));
+		
+		
+		for (int i = 0; i <= alpha.length() - 1; i++) {
+			//alternate: i < alpha.length();
+			
+			System.out.println(alpha.charAt(i));
+		}
+		
+		// delete the * / to activate 
+		// */ //prints all of alpha per character, as well as first/last characters of alpha
+		
+		for (int i = 0; i<= vanillaText.length() - 1; i++) {
+			
+			System.out.println(alpha.indexOf(vanillaText.charAt(i)));
+			chocolateText = chocolateText + alpha.charAt(alpha.indexOf(vanillaText.charAt(i) + key));
+		}
+		
+		/**
+		 * Each car that I need from vanillaText I find the index in alpha
+		 * for example, reed A to look up index for A, get 0
+		 * Add key to index and write to chocolateText
+		 */
+		
+		
+		return chocolateText;
+		
+	}
+
 }
--- src/caesar/CipherRunHere.java
+++ src/caesar/CipherRunHere.java
@@ -1,10 +1,32 @@
 package caesar;
 
+import javax.swing.JOptionPane;
+
 public class CipherRunHere {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String vanillaText;
+		String chocolateText;
+		int key = 1;
+		
+		CaesarCipher activeCipher = new CaesarCipher();
+		
+		vanillaText = JOptionPane.showInputDialog("Enter text to encode: ");
+		
+		do {
+			try {
+				key = Integer.valueOf(JOptionPane.showInputDialog("Enter encoding key: "));
+				break;
+			} catch (Exception err){
+				JOptionPane.showMessageDialog(null, "Warning! " + err + "... Key must be a whole number.");
+			}
+		} while (true); //proud of this
+		
+		
+		chocolateText = activeCipher.encode(vanillaText, key);
+		
+		JOptionPane.showMessageDialog(null, "Encoded message: " + chocolateText);
 	}
 
 }
