c9ca365b235b516fa2bff88a50468445303151f0
compsci <compsci@compsci>
2017-10-24 10:38:49 -0600

    Space checking, as well as optional live key mode

--- src/caesar/CaesarCipher.java
+++ src/caesar/CaesarCipher.java
@@ -3,29 +3,36 @@ package caesar;
 public class CaesarCipher {
 
 	
-	private String alpha = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? ";
+	private String alpha = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?";
 	
 	// "aBcDeFgHiJkLmNoPqRsTuVwXyZAbCdEfGhIjKlMnOpQrStUvWxYz 0123456789"
 	// "abCDefGHijKLmnOPqrSTuvWXyzABcdEFghIJklMNopQRstUVwxYZ 0123456789"
 	// "1234567890qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM"
 	// "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? "
-	
-	
+
 	public String encode(String vanillaText, int key) {
 		
 		String chocolateText = "";
+		Boolean liveUpdatingKey = false;
+		int updatingKey = key;
 		
 		
 		
 		for (int i = 0; i<= vanillaText.length() - 1; i++) {
 			
 			System.out.println(alpha.indexOf(vanillaText.charAt(i)));
-			chocolateText = chocolateText + alpha.charAt(Math.abs((alpha.indexOf(vanillaText.charAt(i)) + key) % alpha.length()));
+			if(vanillaText.charAt(i) != ' ') {
+				chocolateText = chocolateText + alpha.charAt(Math.abs((alpha.indexOf(vanillaText.charAt(i)) + updatingKey) % alpha.length()));
+			} else {
+				chocolateText = chocolateText + " ";
+				if (liveUpdatingKey == true) {
+					updatingKey = updatingKey + key;
+				}
+			}
+			
 		}
-		
-		
-		
-		
+	
+
 		return chocolateText;
 		
 	}
