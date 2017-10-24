9ab26898d157d88c58b46cc6f0fbf88ecc486fc9
compsci <compsci@compsci>
2017-10-24 10:14:08 -0600

    whole keyboard support

--- src/caesar/CaesarCipher.java
+++ src/caesar/CaesarCipher.java
@@ -3,11 +3,12 @@ package caesar;
 public class CaesarCipher {
 
 	
+	private String alpha = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? ";
 	
-	private String alpha = "abCDefGHijKLmnOPqrSTuvWXyzABcdEFghIJklMNopQRstUVwxYZ ";
-	
-	// "aBcDeFgHiJkLmNoPqRsTuVwXyZAbCdEfGhIjKlMnOpQrStUvWxYz "
-	// "abCDefGHijKLmnOPqrSTuvWXyzABcdEFghIJklMNopQRstUVwxYZ "
+	// "aBcDeFgHiJkLmNoPqRsTuVwXyZAbCdEfGhIjKlMnOpQrStUvWxYz 0123456789"
+	// "abCDefGHijKLmnOPqrSTuvWXyzABcdEFghIJklMNopQRstUVwxYZ 0123456789"
+	// "1234567890qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM"
+	// "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? "
 	
 	
 	public String encode(String vanillaText, int key) {
@@ -32,7 +33,7 @@ public class CaesarCipher {
 		for (int i = 0; i<= vanillaText.length() - 1; i++) {
 			
 			System.out.println(alpha.indexOf(vanillaText.charAt(i)));
-			chocolateText = chocolateText + alpha.charAt((alpha.indexOf(vanillaText.charAt(i)) + key) % alpha.length());
+			chocolateText = chocolateText + alpha.charAt(Math.abs((alpha.indexOf(vanillaText.charAt(i)) + key) % alpha.length()));
 		}
 		
 		/**
