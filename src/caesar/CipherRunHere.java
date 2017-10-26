5b0ea52d74add9bc6dafbc160133e2c97f1c1283
compsci <compsci@compsci>
2017-10-26 10:14:37 -0600

    Rebuilt runhere to have selection boxes for encoder vs decoder, etc.

--- src/caesar/CipherRunHere.java
+++ src/caesar/CipherRunHere.java
@@ -4,32 +4,56 @@ import javax.swing.JOptionPane;
 
 public class CipherRunHere {
 
-	
+	static Object[] selections = {"Encoder","Decoder", "Cracker", "None"};
+	static Object[] LKSelections = {"Disabled","Enabled"};
+	static String selection;
 	
 	public static void main(String[] args) {
 		
-		String vanillaText;
-		String chocolateText;
-		int key = 1;
 		
-		CaesarCipher activeCipher = new CaesarCipher();
+		selection = (String) JOptionPane.showInputDialog(null, "Select a utility:", "Util select", JOptionPane.PLAIN_MESSAGE, null, selections, selection);
 		
-		vanillaText = JOptionPane.showInputDialog("Enter text to encode: ");
-		
-		do {
-			try {
-				key = Integer.valueOf(JOptionPane.showInputDialog("Enter encoding key: "));
-				break;
-			} catch (Exception err){
-				JOptionPane.showMessageDialog(null, "Warning! " + err + "... Key must be a whole number.");
+		if (selection.equals("Encoder") || selection.equals("Decoder")) {
+			
+			String LKSelection = (String) JOptionPane.showInputDialog(null, "LiveKey mode enabled?", "Mode select", JOptionPane.PLAIN_MESSAGE, null, LKSelections, selection);
+			
+			String vanillaText = "";
+			String chocolateText;
+			int key = 1;
+			
+			CaesarCipher activeCipher = new CaesarCipher();
+			
+			while (vanillaText.equals("")) {
+				
+				vanillaText = JOptionPane.showInputDialog("Enter text to encode: ");
 			}
-		} while (true); //proud of this
-		
-		
-		chocolateText = activeCipher.encode(vanillaText, key, true);
-		
-		JOptionPane.showMessageDialog(null, "Encoded message: " + chocolateText);
-		System.out.println(chocolateText);
+			
+			do {
+				try {
+					key = Integer.valueOf(JOptionPane.showInputDialog("Enter key: "));
+					
+					if (selection.equals("Decoder") ) {
+						key = -key;
+					}
+					
+					break;
+					
+				} catch (Exception err){
+					
+					JOptionPane.showMessageDialog(null, "Warning! " + err + "... Key must be a whole number.");
+				}
+			} while (true); //proud of this
+			
+			
+			
+			if (LKSelection.equals("Enabled")) {
+				chocolateText = activeCipher.encode(vanillaText, key, true);
+			} else {
+				chocolateText = activeCipher.encode(vanillaText, key, false);
+			}
+				
+			JOptionPane.showMessageDialog(null, "Encoded message: " + chocolateText);
+			System.out.println(chocolateText);
+		}
 	}
-
 }
